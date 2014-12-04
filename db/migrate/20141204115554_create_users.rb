463d1b98f03b33e357c0c40143e7c2831be1575b
Simone Vittori <hello@simonewebdesign.it>
2014-12-04 13:01:44 +0100

    Update migration

--- db/migrate/20141204115554_create_users.rb
+++ db/migrate/20141204115554_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :handle
+      t.string :handle, limit: 50
       t.integer :followers
 
       t.timestamps null: false
