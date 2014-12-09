150be8e80b2c7eda271a65377f83cbf8adc699e3
Simone Vittori <hello@simonewebdesign.it>
2014-12-09 15:48:47 +0100

    Display sentiment with images instead of numbers

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,4 +4,15 @@ module TweetsHelper
     return true if message =~ /(coke|coca-cola|diet cola)/i
     false
   end
+
+  def sentiment_image_tag(score)
+    case
+    when score > 0
+      image_tag 'sentiment/positive.png'
+    when score < 0
+      image_tag 'sentiment/negative.png'
+    else
+      image_tag 'sentiment/neutral.png'
+    end
+  end
 end
