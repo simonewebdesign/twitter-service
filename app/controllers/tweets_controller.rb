5159b6bc12329a99a5260e6f89b8ab7c0aa81c60
Simone Vittori <hello@simonewebdesign.it>
2014-12-09 16:21:51 +0100

    Tidy up tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,6 @@ class TweetsController < ApplicationController
   rescue_from TweetsFetchJobError::InvalidResponse, with: :show_error
 
   # GET /tweets
-  # GET /tweets.json
   def index
     # Retrieve count of unique tweets
     @tweets_count = Tweet.unscoped.group(:remote_id).count(:id)
@@ -18,7 +17,6 @@ class TweetsController < ApplicationController
   end
 
   # GET /tweets/1
-  # GET /tweets/1.json
   def show
   end
 
@@ -32,49 +30,34 @@ class TweetsController < ApplicationController
   end
 
   # POST /tweets
-  # POST /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
 
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
-      else
-        format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
+    if @tweet.save
+      redirect_to @tweet, notice: 'Tweet was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /tweets/1
-  # PATCH/PUT /tweets/1.json
   def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tweet }
-      else
-        format.html { render :edit }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
+    if @tweet.update(tweet_params)
+      redirect_to @tweet, notice: 'Tweet was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /tweets/1
-  # DELETE /tweets/1.json
   def destroy
     @tweet.destroy
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to tweets_url, notice: 'Tweet was successfully destroyed.'
   end
 
   def tweets_from_api
     TweetsFetchJob.perform_later
-
-    redirect_to tweets_path
+    redirect_to tweets_path, notice: 'Tweets fetched successfully.'
   end
 
   protected
@@ -89,7 +72,7 @@ class TweetsController < ApplicationController
       @tweet = Tweet.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def tweet_params
       params.require(:tweet).permit(:message, :sentiment, :remote_id, :user_id)
     end
